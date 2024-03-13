a5c6f5a93379ecb9452cfdf08ce2e33790af9d89
Peter Pacholarz <116585257+peterPacho@users.noreply.github.com>
2024-03-12 20:35:38 -0500

    auto-formatted and quickly added mouse camera control

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject mainCamera;
+
+    [SerializeField]
+    private float mouseSensitivity = 2.0f;
+
     public GameObject Cube3; // Assign the Ball GameObject in the inspector
- public float speed=10;
- public float turnspeed=5;
- public float HorizontalInput;
- public float VerticalInput;
+    public float speed = 10;
+    public float turnspeed = 5;
+    public float HorizontalInput;
+    public float VerticalInput;
     void Update()
     {
-        HorizontalInput=Input.GetAxis("Horizontal");
-        VerticalInput=Input.GetAxis("Vertical");
-    transform.Rotate(Vector3.up*Time.deltaTime*speed*HorizontalInput);
-    
-    transform.Rotate(Vector3.right*Time.deltaTime*speed*VerticalInput);
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.forward * Time.deltaTime * speed * HorizontalInput);
+
+        transform.Rotate(Vector3.right * Time.deltaTime * speed * VerticalInput);
+
+        if (Input.GetMouseButton(0))
+        {
+            mainCamera.transform.Rotate(Input.GetAxis("Mouse Y") * -mouseSensitivity, Input.GetAxis("Mouse X") * mouseSensitivity, 0);
+        }
         // if (Input.GetKeyDown(KeyCode.X))
         // {
         //     // Rotate the Cube around its X axis
